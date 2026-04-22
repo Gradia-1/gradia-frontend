cf5dce6267bae4897b91322b2c5289b4ae8d34ce
uwitonze8 <pacific.uwitonze112@gmail.com>
2026-04-22 21:42:43 +0200

    new changes on the gradia frontend

--- app/pages/pricing.tsx
+++ app/pages/pricing.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import Navbar from "../components/navabar";
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 type BillingPeriod = "monthly" | "sixMonth" | "yearly";
